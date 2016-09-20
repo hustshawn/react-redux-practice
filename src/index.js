ccae205547d2f0d38d74c4e20c2bbb9c866323c7
Shawn Zhang <hustshawn@gmail.com>
2016-09-20 15:24:42 +0800

    Applied the browserHistory instead of HashHistory for react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import { browserHistory } from 'react-router'
+// import { hashHistory } from 'react-router'
 
 import configreStore from './store'
 import App from './components/App'
@@ -10,7 +13,9 @@ import App from './components/App'
 const store = configreStore()
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={ browserHistory } >
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
