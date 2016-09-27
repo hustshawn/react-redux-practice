8ba1375699eca8271980dd5f29ec0c6c12ad6174
hustshawn <hustshawn@gmail.com>
2016-09-27 22:13:52 +0800

    Added browser history into the Nav componnet

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton';
 
 import LinkButton from './LinkButton'
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import { cyan500 } from 'material-ui/styles/colors'
+import { Link } from 'react-router'
+import {List, ListItem, MakeSelectable} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import Avatar from 'material-ui/Avatar';
+import { browserHistory } from 'react-router'
+
+
+let SelectableList = MakeSelectable(List)
+
 class Nav extends React.Component {
   constructor(props) {
     super(props);
@@ -17,15 +28,36 @@ class Nav extends React.Component {
     })
   }
 
+  handleMenuItemClick = () => {
+    this.setState({
+      open: false
+    })
+  }
+  handleRequestChangeList = (event, value) => {
+    browserHistory.push(value)
+    this.setState({
+      open: false
+    })
+  }
   getStyle = () => ({
     title: {
       paddingLeft: 20
     },
+    itemButton: {
+      width: '100%',
+    marginLeft: 0,
+    paddingLeft: 16,
+    paddingRight: 16,
+    position: 'relative'
+      // opacity:0
+    }
   })
+
    
   render() {
     const style = this.getStyle()
     const title = 'App Bar'
+    console.log(this)
     return (
      <div>
         <AppBar 
@@ -42,15 +74,28 @@ class Nav extends React.Component {
         />
         <Drawer 
           open={this.state.open} 
-          docked={false} 
+          docked={true} 
           onRequestChange={(open, reason) => {
             this.setState({open:false})
             console.log(reason)
           }}
         >
           <AppBar title={title} />
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
+          <SelectableList
+            value={location.pathname}
+            onChange={this.handleRequestChangeList}
+           >
+            <Subheader>Selectable Contacts</Subheader>
+            <ListItem
+              value={"companies"}
+              primaryText="Companies"
+            />
+            <ListItem
+              value={"todos"}
+              primaryText="Todos"
+            />
+
+          </SelectableList>
         </Drawer>
      </div>
 
