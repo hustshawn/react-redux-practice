2ae313f31a566192f0da9e4e19aa54703bd7fffa
Shawn Zhang <hustshawn@gmail.com>
2016-09-28 18:35:52 +0800

    Implemented all functions for Nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,205 @@
-import React from 'react'
-
-import Nav from './Nav'
-import '../index.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, {PropTypes} from 'react'
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { browserHistory } from 'react-router'
+
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer';
+import {darkWhite, lightWhite, grey900, cyan500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {List, ListItem, MakeSelectable} from 'material-ui/List';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {spacing, typography, zIndex} from 'material-ui/styles';
+import Subheader from 'material-ui/Subheader';
+import withWidth, {MEDIUM, LARGE} from 'material-ui/utils/withWidth';
+
+import '../index.css';
 import '../App.css'
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-export default class App extends React.Component {
+let SelectableList = MakeSelectable(List)
+const styles = {
+  logo: {
+    cursor: 'pointer',
+    fontSize: 24,
+    color: typography.textFullWhite,
+    lineHeight: `${spacing.desktopKeylineIncrement}px`,
+    fontWeight: typography.fontWeightLight,
+    backgroundColor: cyan500,
+    paddingLeft: spacing.desktopGutter,
+    marginBottom: 8,
+  },
+  version: {
+    paddingLeft: spacing.desktopGutterLess,
+    fontSize: 16,
+  },
+};
+
+class App extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+    location: PropTypes.object,
+    width: PropTypes.number.isRequired,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      navDrawerOpen: false,
+      muiTheme: getMuiTheme()
+    };
+  }
+
+  getStyles() {
+    const styles = {
+      appBar: {
+        position: 'fixed',
+        // Needed to overlap the examples
+        zIndex: this.state.muiTheme.zIndex.appBar + 1,
+        top: 0,
+      },
+      root: {
+        paddingTop: spacing.desktopKeylineIncrement,
+        minHeight: 400,
+      },
+      content: {
+        margin: spacing.desktopGutter,
+      },
+      contentWhenMedium: {
+        margin: `${spacing.desktopGutter * 2}px ${spacing.desktopGutter * 3}px`,
+      },
+      footer: {
+        backgroundColor: grey900,
+        textAlign: 'center',
+      },
+      a: {
+        color: darkWhite,
+      },
+      p: {
+        margin: '0 auto',
+        padding: 0,
+        color: lightWhite,
+        maxWidth: 356,
+      },
+      browserstack: {
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        margin: '25px 15px 0',
+        padding: 0,
+        color: lightWhite,
+        lineHeight: '25px',
+        fontSize: 12,
+      },
+      browserstackLogo: {
+        margin: '0 3px',
+      },
+      iconButton: {
+        color: darkWhite,
+      },
+    }
+
+    if (this.props.width === MEDIUM || this.props.width === LARGE) {
+      styles.content = Object.assign(styles.content, styles.contentWhenMedium);
+    }
+
+    return styles;
+  }
+
+  handleLeftIconButtonTouchTap = () => {
+    this.setState({
+      navDrawerOpen: !this.state.navDrawerOpen
+    })
+  }
+
+  handleRequestChangeList = (event, value) => {
+    browserHistory.push(value)
+    this.setState({
+      navDrawerOpen: false
+    })
+  }
+
+  handleOnRequestChangeNavDrawer = () => {
+    this.setState({
+      navDrawerOpen: false
+    })
+  }
+  handleDrawTitleTouchTap = () => {
+    browserHistory.push('/')
+  }
 
   render() {
+    const {
+      location,
+      children,
+    } = this.props;
+    let {
+      navDrawerOpen,
+    } = this.state;
 
+    const {
+      prepareStyles,
+    } = this.state.muiTheme;
+    const styles = this.getStyles();
+
+    const title = "App Bar"
+    let docked = false
+    let showMenuIconButton = true
+    console.log(this.props)
+    if (this.props.width === LARGE && title !== '') {
+      docked = true;
+      navDrawerOpen = true;
+      showMenuIconButton = false;
+
+      styles.navDrawer = {
+        zIndex: styles.appBar.zIndex - 1,
+      };
+      styles.root.paddingLeft = 256;
+      styles.footer.paddingLeft = 256;
+    }
+    console.log(navDrawerOpen)
     return (
       <MuiThemeProvider>
         <div className="HolyGrail">
-          <Nav className="HolyGrail-nav"/>
-            <div className="HolyGrail-content" style={{margin: 50}} >
-              {this.props.children}
-            </div>
+          <AppBar 
+            onLeftIconButtonTouchTap={this.handleLeftIconButtonTouchTap}
+            showMenuIconButton={showMenuIconButton}
+            title={title}
+            zDepth={0}
+          />
+          <Drawer
+            open={navDrawerOpen}
+            docked={docked}
+            onRequestChange={this.handleOnRequestChangeNavDrawer}
+            containerStyle={{zIndex: zIndex.drawer - 100}}
+            >
+            <AppBar 
+              title={title}
+              showMenuIconButton={false}
+              onTitleTouchTap={this.handleDrawTitleTouchTap} />
+            <SelectableList
+              value={location.pathname}
+              onChange={this.handleRequestChangeList}
+            >
+              <Subheader>Selectable Contacts</Subheader>
+              <ListItem
+                value={"companies"}
+                primaryText="Companies"
+              />
+              <ListItem
+                value={"todos"}
+                primaryText="Todos App"
+              />
+            </SelectableList>
+          </Drawer>
+
+          <div className="HolyGrail-content" style={{margin: 50}} >
+            {this.props.children}
+          </div>
         </div>
       </MuiThemeProvider>
     )
   }
 }
+
+export default withWidth()(App)
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton';
-
+import spacing from 'material-ui/styles/spacing';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import { cyan500 } from 'material-ui/styles/colors'
@@ -11,19 +11,25 @@ import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import { browserHistory } from 'react-router'
-
+import withWidth, {MEDIUM, LARGE} from 'material-ui/utils/withWidth';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import zIndex from 'material-ui/styles/zIndex'
+import {darkWhite, lightWhite, grey900} from 'material-ui/styles/colors';
 
 let SelectableList = MakeSelectable(List)
 
 class Nav extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {open: false,
+      muiTheme: getMuiTheme()
+    };
   }
 
   handleLeftIconButtonTouchTap = () => {
     this.setState({
-      open: !this.state.open
+      open: !this.state.open,
     })
   }
 
@@ -41,8 +47,22 @@ class Nav extends React.Component {
   }
 
   getStyle = () => ({
+    appBar: {
+      position: 'fixed',
+      // Needed to overlap the examples
+      zIndex: zIndex.appBar + 1,
+      top: 0,
+    },
     title: {
       paddingLeft: 20
+    },
+    root: {
+      paddingTop: spacing.desktopKeylineIncrement,
+      minHeight: 400,
+    },
+    footer: {
+      backgroundColor: grey900,
+      textAlign: 'center',
     },
     itemButton: {
       width: '100%',
@@ -58,21 +78,49 @@ class Nav extends React.Component {
   render() {
     const style = this.getStyle()
     const title = 'App Bar'
+    console.log(this.state)
+    let docked = false;
+    let showMenuIconButton = true;
+    const {
+      prepareStyles,
+    } = this.state.muiTheme;
+
+    if (this.props.width === LARGE && title !== '') {
+      docked = true;
+      navDrawerOpen = true;
+      showMenuIconButton = false;
+
+      style.navDrawer = {
+        zIndex: style.appBar.zIndex - 1,
+      };
+      style.root.paddingLeft = 256;
+      style.footer.paddingLeft = 256;
+    }
     return (
      <div>
+       
         <AppBar 
         onLeftIconButtonTouchTap={this.handleLeftIconButtonTouchTap}
         showMenuIconButton={true}
         title={title}
         zDepth={0}
         />
+           {title !== '' ?
+          <div style={prepareStyles(styles.root)}>
+            <div style={prepareStyles(styles.content)}>
+              {React.cloneElement(children, {
+                onChangeMuiTheme: this.handleChangeMuiTheme,
+              })}
+            </div>
+          </div> :
+          children
+        }
         <Drawer 
           open={this.state.open} 
           docked={false} 
           onRequestChange={(open, reason) => {
             this.setState({open:false})
-          }}
-        >
+          }}>
           <AppBar title={title} />
           <SelectableList
             value={location.pathname}
